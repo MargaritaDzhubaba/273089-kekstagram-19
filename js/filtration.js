77ded37730bd2380fd125bd655391a891355ed58
MargaritaDzhubaba <rikajubaba@gmail.com>
2020-03-17 02:18:03 +0300

    Исправила ошибки с debounce

--- js/filtration.js
+++ js/filtration.js
@@ -5,16 +5,13 @@
   var RANDOM_PHOTOS_NUMBER = 10;
 
   var imgFilters = document.querySelector('.img-filters');
-  var filtersButton = imgFilters.querySelectorAll('.img-filters__button');
-  var filterDefault = imgFilters.querySelector('#filter-default');
-  var filterRandom = imgFilters.querySelector('#filter-random');
-  var filterDiscussed = imgFilters.querySelector('#filter-discussed');
+  var filterButtons = imgFilters.querySelectorAll('.img-filters__button');
 
-  filtersButton.forEach(function (evt) {
-    evt.addEventListener('click', function () {
+  filterButtons.forEach(function (item) {
+    item.addEventListener('click', function () {
       var activeButton = imgFilters.querySelector('.img-filters__button--active');
       activeButton.classList.remove('img-filters__button--active');
-      evt.classList.add('img-filters__button--active');
+      item.classList.add('img-filters__button--active');
     });
   });
 
@@ -23,7 +20,7 @@
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < photos.length; i++) {
-      fragment.appendChild(window.creationElements.renderPicture(photos[i]));
+      fragment.appendChild(window.elementsCreation.renderPicture(photos[i]));
     }
     var galleryPhotos = document.querySelectorAll('.picture');
     galleryPhotos.forEach(function (item) {
@@ -36,33 +33,49 @@
     data.forEach(function (photo, index) {
       photo.id = index + 1;
     });
-    window.creationElements.photosData = data;
+    window.elementsCreation.photosData = data;
     renderPhotos(data);
-
-    filterDefault.addEventListener('click', window.util.debounce(function () {
-      renderPhotos(data);
-    }));
-
-    filterRandom.addEventListener('click', window.util.debounce(function () {
-      var photosRandom = data.slice(0, RANDOM_PHOTOS_NUMBER).sort(function () {
-        return window.util.getRundomNumber(data.length);
-      });
-      renderPhotos(photosRandom);
-    }));
-
-    filterDiscussed.addEventListener('click', window.util.debounce(function () {
-      var photosDiscussed = data.slice().sort(function (first, second) {
-        if (second.comments.length > first.comments.length) {
-          return 1;
-        } else if (second.comments.length < first.comments.length) {
-          return -1;
-        } else {
-          return 0;
-        }
-      });
-      renderPhotos(photosDiscussed);
-    }));
   };
+
+  var filterDefault = function () {
+    renderPhotos(window.elementsCreation.photosData);
+  };
+
+  var filterRandom = function () {
+    var photosRandom = window.elementsCreation.photosData.slice(0, RANDOM_PHOTOS_NUMBER).sort(function () {
+      return window.util.getRundomNumber(window.elementsCreation.photosData.length);
+    });
+    renderPhotos(photosRandom);
+  };
+
+  var filterDiscussed = function () {
+    var photosDiscussed = window.elementsCreation.photosData.slice().sort(function (first, second) {
+      if (second.comments.length > first.comments.length) {
+        return 1;
+      } else if (second.comments.length < first.comments.length) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    renderPhotos(photosDiscussed);
+  };
+
+  var filterPhotos = window.util.debounce(function (evt) {
+    var target = evt.target;
+
+    if (target.id === 'filter-default') {
+      filterPhotos = filterDefault();
+    } else if (target.id === 'filter-random') {
+      filterPhotos = filterRandom();
+    } else if (target.id === 'filter-discussed') {
+      filterPhotos = filterDiscussed();
+    }
+  });
+
+  filterButtons.forEach(function (item) {
+    item.addEventListener('click', filterPhotos);
+  });
 
   var onError = function (errorMessage) {
     var node = document.createElement('div');
